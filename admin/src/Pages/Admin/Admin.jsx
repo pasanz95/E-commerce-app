36afab54dafbad10fd366bdfa21b908662c23ef2
Pasan sadeepa <pasansadeepa222@gmail.com>
2024-03-02 15:40:04 +0530

    files added

---
+++ admin/src/Pages/Admin/Admin.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Admin.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import { Routes,Route } from 'react-router-dom'
+import AddProducts from '../../Components/AddProducts/AddProducts'
+import ListProduct from '../../Components/ListProduct/ListProduct'
+
+
+const Admin = () => {
+  return (
+    <div className='admin'>
+        <Sidebar/>
+        <Routes>
+        <Route path='/addproduct' element={<AddProducts/>}/>
+        <Route path='/listproduct' element={<ListProduct/>}/>
+        
+           
+        </Routes>
+    </div>
+  )
+}
+
+export default Admin
